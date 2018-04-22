70fe3dd1756a584d37022f240731cbf1256a0ad1
DN2 <david@sleepers.pro>
2018-04-22 14:19:03 -0700

    Test fixes and variable cleanup.

--- core/poller/cmd/unifi-poller/config.go
+++ core/poller/cmd/unifi-poller/config.go
@@ -28,13 +28,13 @@ const (
 
 // Config represents the data needed to poll a controller and report to influxdb.
 type Config struct {
-	Interval   time.Duration `json:"interval",toml:"interval",yaml:"interval"`
-	InfluxURL  string        `json:"influx_url",toml:"influx_addr",yaml:"influx_addr"`
-	InfluxUser string        `json:"influx_user",toml:"influx_user",yaml:"influx_user"`
-	InfluxPass string        `json:"influx_pass",toml:"influx_pass",yaml:"influx_pass"`
-	InfluxDB   string        `json:"influx_db",toml:"influx_db",yaml:"influx_db"`
-	UnifiUser  string        `json:"unifi_user",toml:"unifi_user",yaml:"unifi_user"`
-	UnifiPass  string        `json:"unifi_pass"toml:"unifi_pass",yaml:"unifi_pass"`
-	UnifiBase  string        `json:"unifi_url",toml:"unifi_url",yaml:"unifi_url"`
+	Interval   time.Duration `json:"interval" toml:"interval" xml:"interval" yaml:"interval"`
+	InfluxURL  string        `json:"influx_url" toml:"influx_addr" xml:"influx_addr" yaml:"influx_addr"`
+	InfluxUser string        `json:"influx_user" toml:"influx_user" xml:"influx_user" yaml:"influx_user"`
+	InfluxPass string        `json:"influx_pass" toml:"influx_pass" xml:"influx_pass" yaml:"influx_pass"`
+	InfluxDB   string        `json:"influx_db" toml:"influx_db" xml:"influx_db" yaml:"influx_db"`
+	UnifiUser  string        `json:"unifi_user" toml:"unifi_user" xml:"unifi_user" yaml:"unifi_user"`
+	UnifiPass  string        `json:"unifi_pass" toml:"unifi_pass" xml:"unifi_pass" yaml:"unifi_pass"`
+	UnifiBase  string        `json:"unifi_url" toml:"unifi_url" xml:"unifi_url" yaml:"unifi_url"`
 	uniClient  *http.Client
 }
