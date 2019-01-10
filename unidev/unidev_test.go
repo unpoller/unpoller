364fede31c79c1e39ccd1c78d738f209eaeceee8
DN2 <david@sleepers.pro>
2019-01-10 01:35:39 -0800

    make the test smaller, because why not.

--- unidev/unidev_test.go
+++ unidev/unidev_test.go
@@ -10,17 +10,15 @@ import (
 func TestFlexInt(t *testing.T) {
 	t.Parallel()
 	a := assert.New(t)
-	five := []byte(`{"channel": "5"}`)
-	seven := []byte(`{"channel": 7}`)
-	auto := []byte(`{"channel": "auto"}`)
 	type reply struct {
 		Channel FlexInt `json:"channel"`
 	}
 	var r reply
-	a.Nil(json.Unmarshal(five, &r))
+	a.Nil(json.Unmarshal([]byte(`{"channel": "5"}`), &r))
 	a.EqualValues(FlexInt(5), r.Channel)
-	a.Nil(json.Unmarshal(seven, &r))
+	a.Nil(json.Unmarshal([]byte(`{"channel": 7}`), &r))
 	a.EqualValues(FlexInt(7), r.Channel)
-	a.Nil(json.Unmarshal(auto, &r), "a regular string must not produce an unmarshal error")
+	a.Nil(json.Unmarshal([]byte(`{"channel": "auto"}`), &r),
+		"a regular string must not produce an unmarshal error")
 	a.EqualValues(FlexInt(0), r.Channel)
 }
