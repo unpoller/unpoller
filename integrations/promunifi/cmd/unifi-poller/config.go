978ae5b0c16535e1d43773eecd5b47376eed4e70
DN2 <david@sleepers.pro>
2019-01-11 21:08:58 -0800

    Move flags to config file.

--- integrations/promunifi/cmd/unifi-poller/config.go
+++ integrations/promunifi/cmd/unifi-poller/config.go
@@ -3,23 +3,29 @@ package main
 import "time"
 
 // Version is loosely followed.
-var Version = "v0.2.1"
+var Version = "v0.3.0"
 
 const (
 	// App defaults in case they're missing from the config.
-	defaultConfFile = "/usr/local/etc/unifi-poller/up.conf"
-	defaultInterval = 30 * time.Second
-	defaultInfxDb   = "unifi"
-	defaultInfxUser = "unifi"
-	defaultInfxPass = "unifi"
-	defaultInfxURL  = "http://127.0.0.1:8086"
-	defaultUnifUser = "influx"
-	defaultUnifURL  = "https://127.0.0.1:8443"
+	defaultConfFile  = "/usr/local/etc/unifi-poller/up.conf"
+	defaultInterval  = 30 * time.Second
+	defaultInfxDb    = "unifi"
+	defaultInfxUser  = "unifi"
+	defaultInfxPass  = "unifi"
+	defaultInfxURL   = "http://127.0.0.1:8086"
+	defaultUnifUser  = "influx"
+	defaultUnifURL   = "https://127.0.0.1:8443"
+	defaultVerifySSL = false
+	defaultDebug     = false
+	defaultQuiet     = false
 )
 
 // Config represents the data needed to poll a controller and report to influxdb.
 type Config struct {
 	Interval   Dur    `json:"interval" toml:"interval" xml:"interval" yaml:"interval"`
+	Debug      bool   `json:"debug" toml:"debug" xml:"debug" yaml:"debug"`
+	Quiet      bool   `json:"quiet" toml:"quiet" xml:"quiet" yaml:"quiet"`
+	VerifySSL  bool   `json:"verify_ssl" toml:"verify_ssl" xml:"verify_ssl" yaml:"verify_ssl"`
 	InfluxURL  string `json:"influx_url" toml:"influx_url" xml:"influx_url" yaml:"influx_url"`
 	InfluxUser string `json:"influx_user" toml:"influx_user" xml:"influx_user" yaml:"influx_user"`
 	InfluxPass string `json:"influx_pass" toml:"influx_pass" xml:"influx_pass" yaml:"influx_pass"`
