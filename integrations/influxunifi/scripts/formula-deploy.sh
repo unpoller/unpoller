b2519424d79c6dce40995086f8c33cc6c314d386
David Newhall II <david@sleepers.pro>
2019-06-13 01:46:17 -0700

    Fix homebrew build.

--- integrations/influxunifi/scripts/formula-deploy.sh
+++ integrations/influxunifi/scripts/formula-deploy.sh
@@ -5,13 +5,13 @@
 make unifi-poller.rb
 VERSION=$(grep -E '^\s*version\s*"' unifi-poller.rb | cut -d\" -f 2)
 
-rm -rf homebrew-repo
+rm -rf homebrew-mugs
 git config --global user.email "unifi@auto.releaser"
 git config --global user.name "unifi-auto-releaser"
-git clone git@github.com:golift/homebrew-repo.git
+git clone git@github.com:golift/homebrew-mugs.git
 
-cp unifi-poller.rb homebrew-repo/Formula
-pushd homebrew-repo
+cp unifi-poller.rb homebrew-mugs/Formula
+pushd homebrew-mugs
 echo "Showing diff:"
 git diff
 git commit -m "Update unifi-poller on Release: v${VERSION}" Formula/unifi-poller.rb
