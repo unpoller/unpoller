3ffb658a4aeb39656f9930e50e229d863551cb1b
David Newhall II <david@sleepers.pro>
2019-06-13 01:07:56 -0700

    Rename u

--- integrations/inputunifi/cmd/unifi-poller/start.go
+++ integrations/inputunifi/cmd/unifi-poller/start.go
@@ -9,16 +9,16 @@ import (
 )
 
 func main() {
-	u := &unifipoller.UnifiPoller{}
-	if u.ParseFlags(os.Args[1:]); u.ShowVer {
+	unifi := &unifipoller.UnifiPoller{}
+	if unifi.ParseFlags(os.Args[1:]); unifi.ShowVer {
 		fmt.Printf("unifi-poller v%s\n", unifipoller.Version)
 		return // don't run anything else w/ version request.
 	}
-	if err := u.GetConfig(); err != nil {
-		u.Flag.Usage()
-		log.Fatalf("[ERROR] config file '%v': %v", u.ConfigFile, err)
+	if err := unifi.GetConfig(); err != nil {
+		unifi.Flag.Usage()
+		log.Fatalf("[ERROR] config file '%v': %v", unifi.ConfigFile, err)
 	}
-	if err := u.Run(); err != nil {
+	if err := unifi.Run(); err != nil {
 		log.Fatalln("[ERROR]", err)
 	}
 }
