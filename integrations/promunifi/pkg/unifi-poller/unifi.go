0d8fd00439418f6961174553ddab67439cd9b66a
David Newhall II <david@sleepers.pro>
2019-06-14 21:13:55 -0700

    Add site metrics to influxdb.

--- integrations/promunifi/pkg/unifi-poller/unifi.go
+++ integrations/promunifi/pkg/unifi-poller/unifi.go
@@ -40,95 +40,100 @@ FIRST:
 func (u *UnifiPoller) PollController() {
 	log.Println("[INFO] Everything checks out! Poller started, interval:", u.Interval.value)
 	ticker := time.NewTicker(u.Interval.value)
-
+	var err error
 	for range ticker.C {
+		m := &Metrics{}
 		// Get the sites we care about.
-		sites, err := u.filterSites(u.Sites)
-		if err != nil {
+		if m.Sites, err = u.GetFilteredSites(); err != nil {
 			logErrors([]error{err}, "uni.GetSites()")
 		}
 		// Get all the points.
-		clients, err := u.GetClients(sites)
-		if err != nil {
+		if m.Clients, err = u.GetClients(m.Sites); err != nil {
 			logErrors([]error{err}, "uni.GetClients()")
 		}
-		devices, err := u.GetDevices(sites)
-		if err != nil {
+		if m.Devices, err = u.GetDevices(m.Sites); err != nil {
 			logErrors([]error{err}, "uni.GetDevices()")
 		}
+
 		// Make a new Points Batcher.
-		bp, err := influx.NewBatchPoints(influx.BatchPointsConfig{Database: u.InfluxDB})
+		m.BatchPoints, err = influx.NewBatchPoints(influx.BatchPointsConfig{Database: u.InfluxDB})
 		if err != nil {
 			logErrors([]error{err}, "influx.NewBatchPoints")
 			continue
 		}
 		// Batch (and send) all the points.
-		if errs := batchPoints(devices, clients, bp); errs != nil && hasErr(errs) {
+		if errs := m.SendPoints(); errs != nil && hasErr(errs) {
 			logErrors(errs, "asset.Points()")
 		}
-		if err := u.Write(bp); err != nil {
+		if err := u.Write(m.BatchPoints); err != nil {
 			logErrors([]error{err}, "infdb.Write(bp)")
 		}
 
 		// Talk about the data.
 		var fieldcount, pointcount int
-		for _, p := range bp.Points() {
+		for _, p := range m.Points() {
 			pointcount++
 			i, _ := p.Fields()
 			fieldcount += len(i)
 		}
-		u.Logf("Unifi Measurements Recorded. Sites: %d Clients: %d, "+
+		u.Logf("Unifi Measurements Recorded. Sites: %d, Clients: %d, "+
 			"Wireless APs: %d, Gateways: %d, Switches: %d, Points: %d, Fields: %d",
-			len(sites), len(clients.UCLs),
-			len(devices.UAPs), len(devices.USGs), len(devices.USWs), pointcount, fieldcount)
+			len(m.Sites), len(m.Clients), len(m.UAPs), len(m.USGs), len(m.USWs), pointcount, fieldcount)
 	}
 }
 
-// batchPoints combines all device and client data into influxdb data points.
-func batchPoints(devices *unifi.Devices, clients *unifi.Clients, bp influx.BatchPoints) (errs []error) {
-	process := func(asset Asset) error {
-		if asset == nil {
-			return nil
-		}
-		influxPoints, err := asset.Points()
-		if err != nil {
-			return err
-		}
-		bp.AddPoints(influxPoints)
-		return nil
+// SendPoints combines all device and client data into influxdb data points.
+// Call this after you've collected all the data you care about.
+// This sends all the batched points to InfluxDB.
+func (m *Metrics) SendPoints() (errs []error) {
+	for _, asset := range m.Sites {
+		errs = append(errs, m.processPoints(asset))
 	}
-	if devices != nil {
-		for _, asset := range devices.UAPs {
-			errs = append(errs, process(asset))
-		}
-		for _, asset := range devices.USGs {
-			errs = append(errs, process(asset))
-		}
-		for _, asset := range devices.USWs {
-			errs = append(errs, process(asset))
-		}
+	for _, asset := range m.Clients {
+		errs = append(errs, m.processPoints(asset))
 	}
-	if clients != nil {
-		for _, asset := range clients.UCLs {
-			errs = append(errs, process(asset))
-		}
+	if m.Devices == nil {
+		return
+	}
+	for _, asset := range m.UAPs {
+		errs = append(errs, m.processPoints(asset))
+	}
+	for _, asset := range m.USGs {
+		errs = append(errs, m.processPoints(asset))
+	}
+	for _, asset := range m.USWs {
+		errs = append(errs, m.processPoints(asset))
 	}
 	return
 }
 
-// filterSites returns a list of sites to fetch data for.
-// Omits requested but unconfigured sites.
-func (u *UnifiPoller) filterSites(filter []string) ([]unifi.Site, error) {
+// processPoints is helper function for SendPoints.
+func (m *Metrics) processPoints(asset Asset) error {
+	if asset == nil {
+		return nil
+	}
+	influxPoints, err := asset.Points()
+	if err != nil || influxPoints == nil {
+		return err
+	}
+	m.BatchPoints.AddPoints(influxPoints)
+	return nil
+}
+
+// GetFilteredSites returns a list of sites to fetch data for.
+// Omits requested but unconfigured sites. Grabs the full list from the
+// controller and filters the sites provided in the config file.
+func (u *UnifiPoller) GetFilteredSites() (unifi.Sites, error) {
 	sites, err := u.GetSites()
 	if err != nil {
 		return nil, err
-	} else if len(filter) < 1 || StringInSlice("all", filter) {
+	} else if len(u.Sites) < 1 || StringInSlice("all", u.Sites) {
 		return sites, nil
 	}
 	var i int
 	for _, s := range sites {
 		// Only include valid sites in the request filter.
-		if StringInSlice(s.Name, filter) {
+		if StringInSlice(s.Name, u.Sites) {
 			sites[i] = s
 			i++
 		}
