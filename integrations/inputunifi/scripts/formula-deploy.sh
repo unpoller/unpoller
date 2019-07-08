c7c5073d8514c0c3b88d4940598a2894f3bb1647
David Newhall II <david@sleepers.pro>
2019-07-07 19:54:16 -0700

    fix version on homebrew commit

--- integrations/inputunifi/scripts/formula-deploy.sh
+++ integrations/inputunifi/scripts/formula-deploy.sh
@@ -5,8 +5,7 @@
 # Run by Travis-CI when a new release is created on GitHub.
 
 make unifi-poller.rb
-VERSION=$(grep -E '^\s*version\s*"' unifi-poller.rb | cut -d\" -f 2)
-
+VERSION=$(grep -E '^\s+url\s+"' unifi-poller.rb | cut -d/ -f7 | cut -d. -f1,2,3)
 rm -rf homebrew-mugs
 git config --global user.email "unifi@auto.releaser"
 git config --global user.name "unifi-auto-releaser"
@@ -14,6 +13,6 @@ git clone git@github.com:golift/homebrew-mugs.git
 
 cp unifi-poller.rb homebrew-mugs/Formula
 pushd homebrew-mugs
-git commit -m "Update unifi-poller on Release: v${VERSION}" Formula/unifi-poller.rb
+git commit -m "Update unifi-poller on Release: ${VERSION}" Formula/unifi-poller.rb
 git push
 popd
