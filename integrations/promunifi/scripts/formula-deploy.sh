edf3a3ceb1222ef42070d5690944fce8bbdfdd91
David Newhall II <david@sleepers.pro>
2019-07-07 23:09:21 -0700

    Makefile and formula build updates.

--- integrations/promunifi/scripts/formula-deploy.sh
+++ integrations/promunifi/scripts/formula-deploy.sh
@@ -1,27 +1,28 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# Deploys a new unifi-poller.rb formula file to golift/homebrew-tap.
+# Deploys a new homebrew formula file to golift/homebrew-tap.
 # Requires SSH credentials in ssh-agent to work.
 # Run by Travis-CI when a new release is created on GitHub.
+APP=unpacker-poller
 
 if [ -z "$VERSION" ]; then
   VERSION=$TRAVIS_TAG
 fi
 VERSION=$(echo $VERSION|tr -d v)
 
-make unifi-poller.rb VERSION=$VERSION
+make ${APP}.rb VERSION=$VERSION
 
 if [ -z "$VERSION" ]; then
-  VERSION=$(grep -E '^\s+url\s+"' unifi-poller.rb | cut -d/ -f7 | cut -d. -f1,2,3)
+  VERSION=$(grep -E '^\s+url\s+"' ${APP}.rb | cut -d/ -f7 | cut -d. -f1,2,3)
 fi
 
 rm -rf homebrew-mugs
-git config --global user.email "unifi@auto.releaser"
-git config --global user.name "unifi-auto-releaser"
+git config --global user.email "${APP}@auto.releaser"
+git config --global user.name "${APP}-auto-releaser"
 git clone git@github.com:golift/homebrew-mugs.git
 
-cp unifi-poller.rb homebrew-mugs/Formula
-pushd homebrew-mugs/Formula
-git commit -m "Update unifi-poller on Release: ${VERSION}" unifi-poller.rb
+cp ${APP}.rb homebrew-mugs/Formula
+pushd homebrew-mugs
+git commit -m "Update ${APP} on Release: ${VERSION}" Formula/${APP}.rb
 git push
 popd
