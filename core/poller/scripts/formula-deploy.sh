dc5ade68b3f3c4aea7cf7afa81ef4dbd141b9a35
David Newhall II <david@sleepers.pro>
2019-07-07 21:47:31 -0700

    Fix formula version.

--- core/poller/scripts/formula-deploy.sh
+++ core/poller/scripts/formula-deploy.sh
@@ -4,8 +4,17 @@
 # Requires SSH credentials in ssh-agent to work.
 # Run by Travis-CI when a new release is created on GitHub.
 
-make unifi-poller.rb
-VERSION=$(grep -E '^\s+url\s+"' unifi-poller.rb | cut -d/ -f7 | cut -d. -f1,2,3)
+if [ -z "$VERSION" ]; then
+  VERSION=$TRAVIS_TAG
+fi
+VERSION=$(echo $VERSION|tr -d v)
+
+make unifi-poller.rb VERSION=$VERSION
+
+if [ -z "$VERSION" ]; then
+  VERSION=$(grep -E '^\s+url\s+"' unifi-poller.rb | cut -d/ -f7 | cut -d. -f1,2,3)
+fi
+
 rm -rf homebrew-mugs
 git config --global user.email "unifi@auto.releaser"
 git config --global user.name "unifi-auto-releaser"
