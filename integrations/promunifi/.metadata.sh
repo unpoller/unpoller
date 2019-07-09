16a0f3650b5e532b3e73aaad1c941f3c68cea8c3
David Newhall II <david@sleepers.pro>
2019-07-09 00:47:11 -0700

    more auto

--- integrations/promunifi/.metadata.sh
+++ integrations/promunifi/.metadata.sh
@@ -1,6 +1,8 @@
 # Each line must have an export clause.
 # This file is parsed and sourced by the Makefile, Docker and Homebrew builds.
-TITLE="UniFi Poller"
+
+# Must match the repo name.
+BINARY="unifi-poller"
 # github username
 GHUSER="davidnewhall"
 # docker hub username
@@ -9,19 +11,21 @@ MAINT="David Newhall II <david at sleepers dot pro>"
 VENDOR="Go Lift"
 DESC="Polls a UniFi controller and stores metrics in InfluxDB"
 GOLANGCI_LINT_ARGS="--enable-all -D gochecknoglobals"
+# Example must exist at examples/$CONFIG_FILE.example
 CONFIG_FILE="up.conf"
 LICENSE="MIT"
 
-# The rest if mostly automatic.
-BINARY="$(basename $(git rev-parse --show-toplevel))"
-[ "$BINARY" != "" ] || BINARY="$(basename $(pwd))"
+export BINARY GHUSER DHUSER MAINT VENDOR DESC GOLANGCI_LINT_ARGS CONFIG_FILE LICENSE
+
+# The rest is mostly automatic.
+
 GHREPO="${GHUSER}/${BINARY}"
 URL="https://github.com/${GHREPO}"
 
 # This parameter is passed in as -X to go build. Used to override the Version variable in a package.
 # This makes a path like github.com/davidnewhall/unifi-poller/unifipoller.Version=1.3.3
 # Name the Version-containing library the same as the github repo, without dashes.
-VERSION_PATH="github.com/${GHUSER}/${BINARY}/$(echo ${BINARY} | tr -d -- -).Version"
+VERSION_PATH="github.com/${GHREPO}/$(echo ${BINARY} | tr -d -- -).Version"
 
 # Dynamic. Recommend not changing.
 VERSION="$(git tag -l --merged | tail -n1 | tr -d v || echo development)"
@@ -30,5 +34,4 @@ ITERATION=$(git rev-list --count --all || echo 0)
 DATE="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
 COMMIT="$(git rev-parse --short HEAD || echo 0)"
 
-export TITLE GHUSER DHUSER MAINT VENDOR DESC CONFIG_FILE LICENSE GOLANGCI_LINT_ARGS
-export BINARY GHREPO URL VERSION_PATH VERSION ITERATION DATE COMMIT
+export GHREPO URL VERSION_PATH VERSION ITERATION DATE COMMIT
