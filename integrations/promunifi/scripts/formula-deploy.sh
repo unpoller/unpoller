295433415549ddda6ae5f3d351df4b20cf194c40
David Newhall II <david@sleepers.pro>
2019-07-13 03:52:00 -0700

    Update Library and application-builder code.

--- integrations/promunifi/scripts/formula-deploy.sh
+++ integrations/promunifi/scripts/formula-deploy.sh
@@ -15,6 +15,23 @@ git config --global user.name "${BINARY}-auto-releaser"
 rm -rf homebrew_release_repo
 git clone git@github.com:${HBREPO}.git homebrew_release_repo
 
+# If a bitly token file exists, we'll use that to shorten the link (and allow download counting).
+if [ -f "bitly_token" ]; then
+  API=https://api-ssl.bitly.com/v4/bitlinks
+  # Request payload. In single quotes with double quotes escaped. :see_no_evil:
+  JSON='{\"domain\": \"bit.ly\",\"title\": \"${BINARY}.v${VERSION}-${ITERATION}.tgz\", \
+    \"tags\": [\"${BINARY}\"], \"long_url\": \"https://codeload.github.com/${GHREPO}/tar.gz/v${VERSION}\"}'
+  # Request with headers and data. Using bash -c to hide token from bash -x in travis logs.
+  OUT=$(bash -c "curl -s -X POST -H 'Content-type: application/json' ${API} -H \"\$(<bitly_token)\" -d \"${JSON}\"")
+  # Extract link from reply.
+  LINK="$(echo ${OUT} | jq -r .link | sed 's/http:/https:/')?v=v${VERSION}"
+  # Replace link in formula.
+  sed "s#^  url.*\$#  url \"${LINK}\"#" ${BINARY}.rb > ${BINARY}.rb.new
+  if [ "$?" = "0" ] && [ "$LINK" != "null?v=v${VERSION}" ] && [ "$LINK" != "?v=v${VERSION}" ]; then
+    mv ${BINARY}.rb.new ${BINARY}.rb
+  fi
+fi
+
 cp ${BINARY}.rb homebrew_release_repo/Formula
 pushd homebrew_release_repo
 git add Formula/${BINARY}.rb
