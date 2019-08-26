443fda2eeb3770aa14e4fd87f14d3f9dcfa168b0
David Newhall II <david@sleepers.pro>
2019-08-25 19:01:29 -0700

    Remove struct inheritance.

--- integrations/promunifi/unifipoller/dumper.go
+++ integrations/promunifi/unifipoller/dumper.go
@@ -10,17 +10,17 @@ import (
 
 // DumpJSONPayload prints raw json from the UniFi Controller.
 func (u *UnifiPoller) DumpJSONPayload() (err error) {
-	u.Quiet = true
+	u.Config.Quiet = true
 	u.Unifi, err = unifi.NewUnifi(&unifi.Config{
-		User:      u.UnifiUser,
-		Pass:      u.UnifiPass,
-		URL:       u.UnifiBase,
-		VerifySSL: u.VerifySSL,
+		User:      u.Config.UnifiUser,
+		Pass:      u.Config.UnifiPass,
+		URL:       u.Config.UnifiBase,
+		VerifySSL: u.Config.VerifySSL,
 	})
 	if err != nil {
 		return err
 	}
-	fmt.Fprintln(os.Stderr, "[INFO] Authenticated to UniFi Controller @", u.UnifiBase, "as user", u.UnifiUser)
+	fmt.Fprintln(os.Stderr, "[INFO] Authenticated to UniFi Controller @", u.Config.UnifiBase, "as user", u.Config.UnifiUser)
 	if err := u.CheckSites(); err != nil {
 		return err
 	}
@@ -30,12 +30,12 @@ func (u *UnifiPoller) DumpJSONPayload() (err error) {
 	switch sites, err := u.GetFilteredSites(); {
 	case err != nil:
 		return err
-	case StringInSlice(u.DumpJSON, []string{"d", "device", "devices"}):
+	case StringInSlice(u.Flag.DumpJSON, []string{"d", "device", "devices"}):
 		return u.dumpSitesJSON(unifi.DevicePath, "Devices", sites)
-	case StringInSlice(u.DumpJSON, []string{"client", "clients", "c"}):
+	case StringInSlice(u.Flag.DumpJSON, []string{"client", "clients", "c"}):
 		return u.dumpSitesJSON(unifi.ClientPath, "Clients", sites)
-	case strings.HasPrefix(u.DumpJSON, "other "):
-		apiPath := strings.SplitN(u.DumpJSON, " ", 2)[1]
+	case strings.HasPrefix(u.Flag.DumpJSON, "other "):
+		apiPath := strings.SplitN(u.Flag.DumpJSON, " ", 2)[1]
 		_, _ = fmt.Fprintf(os.Stderr, "[INFO] Dumping Path '%s':\n", apiPath)
 		return u.PrintRawAPIJSON(apiPath)
 	default:
@@ -56,7 +56,7 @@ func (u *UnifiPoller) dumpSitesJSON(path, name string, sites unifi.Sites) error 
 
 // PrintRawAPIJSON prints the raw json for a user-provided path on a UniFi Controller.
 func (u *UnifiPoller) PrintRawAPIJSON(apiPath string) error {
-	body, err := u.GetJSON(apiPath)
+	body, err := u.Unifi.GetJSON(apiPath)
 	fmt.Println(string(body))
 	return err
 }
