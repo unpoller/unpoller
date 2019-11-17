9bd4737730fdf93d8485f06f6baefe208907b942
davidnewhall2 <david@sleepers.pro>
2019-11-17 00:52:13 -0800

    Base layout .. maybe.

---
+++ promunifi/site.go
@@ -0,0 +1,10 @@
+package promunifi
+
+import (
+	"golift.io/unifi"
+)
+
+// exportSite exports Network Site Data
+func (u *unifiCollector) exportSite(s *unifi.Site) []*metricExports {
+	return nil
+}
---
+++ promunifi/udm.go
@@ -0,0 +1,10 @@
+package promunifi
+
+import (
+	"golift.io/unifi"
+)
+
+// exportUDM exports UniFi Dream Machine (and Pro) Data
+func (u *unifiCollector) exportUDM(d *unifi.UDM) []*metricExports {
+	return nil
+}
