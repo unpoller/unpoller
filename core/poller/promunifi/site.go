1135781aa410744535eea0d594f51f2468def086
davidnewhall2 <david@sleepers.pro>
2019-11-17 01:12:33 -0800

    fix a bug and send a few more pieces into the mix

--- core/poller/promunifi/site.go
+++ core/poller/promunifi/site.go
@@ -4,6 +4,13 @@ import (
 	"golift.io/unifi"
 )
 
+type site struct {
+}
+
+func descSite(ns string) *site {
+	return &site{}
+}
+
 // exportSite exports Network Site Data
 func (u *unifiCollector) exportSite(s *unifi.Site) []*metricExports {
 	return nil
