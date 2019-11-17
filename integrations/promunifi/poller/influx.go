9996c8f09652c176d504265b57b06b9e8524f1fe
davidnewhall2 <david@sleepers.pro>
2019-11-17 10:11:58 -0800

    dont need that anymore

--- integrations/promunifi/poller/influx.go
+++ integrations/promunifi/poller/influx.go
@@ -8,6 +8,22 @@ import (
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
+// CollectAndProcess collects measurements and then reports them to InfluxDB
+// Can be called once or in a ticker loop. This function and all the ones below
+// handle their own logging. An error is returned so the calling function may
+// determine if there was a read or write error and act on it. This is currently
+// called in two places in this library. One returns an error, one does not.
+func (u *UnifiPoller) CollectAndProcess() error {
+	metrics, err := u.CollectMetrics()
+	if err != nil {
+		return err
+	}
+	u.AugmentMetrics(metrics)
+	err = u.ReportMetrics(metrics)
+	u.LogError(err, "processing metrics")
+	return err
+}
+
 // ReportMetrics batches all the metrics and writes them to InfluxDB.
 // This creates an InfluxDB writer, and returns an error if the write fails.
 func (u *UnifiPoller) ReportMetrics(metrics *metrics.Metrics) error {
