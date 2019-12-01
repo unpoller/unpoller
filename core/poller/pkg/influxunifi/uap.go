79dae739238424785c741fa715cefdd73dec924d
davidnewhall2 <david@sleepers.pro>
2019-11-30 16:45:22 -0800

    Improve influx library

--- core/poller/pkg/influxunifi/uap.go
+++ core/poller/pkg/influxunifi/uap.go
@@ -1,103 +1,89 @@
 package influxunifi
 
 import (
-	"time"
-
-	influx "github.com/influxdata/influxdb1-client/v2"
 	"golift.io/unifi"
 )
 
-// UAPPoints generates Wireless-Access-Point datapoints for InfluxDB.
+// batchUAP generates Wireless-Access-Point datapoints for InfluxDB.
 // These points can be passed directly to influx.
-func UAPPoints(u *unifi.UAP, now time.Time) ([]*influx.Point, error) {
-	if u.Stat.Ap == nil {
-		u.Stat.Ap = &unifi.Ap{}
+func (u *InfluxUnifi) batchUAP(r report, s *unifi.UAP) {
+	if s.Stat.Ap == nil {
+		s.Stat.Ap = &unifi.Ap{}
 	}
 	tags := map[string]string{
-		"ip":         u.IP,
-		"mac":        u.Mac,
-		"site_id":    u.SiteID,
-		"site_name":  u.SiteName,
-		"name":       u.Name,
-		"cfgversion": u.Cfgversion,
-		"model":      u.Model,
-		"serial":     u.Serial,
-		"type":       u.Type,
+		"mac":       s.Mac,
+		"site_name": s.SiteName,
+		"name":      s.Name,
+		"version":   s.Version,
+		"model":     s.Model,
+		"serial":    s.Serial,
+		"type":      s.Type,
 	}
 	fields := map[string]interface{}{
-		"ip":            u.IP,
-		"bytes":         u.Bytes.Val,
-		"last_seen":     u.LastSeen.Val,
-		"rx_bytes":      u.RxBytes.Val,
-		"tx_bytes":      u.TxBytes.Val,
-		"uptime":        u.Uptime.Val,
-		"state":         u.State,
-		"user-num_sta":  int(u.UserNumSta.Val),
-		"guest-num_sta": int(u.GuestNumSta.Val),
-		"num_sta":       u.NumSta.Val,
-		"loadavg_1":     u.SysStats.Loadavg1.Val,
-		"loadavg_5":     u.SysStats.Loadavg5.Val,
-		"loadavg_15":    u.SysStats.Loadavg15.Val,
-		"mem_buffer":    u.SysStats.MemBuffer.Val,
-		"mem_total":     u.SysStats.MemTotal.Val,
-		"mem_used":      u.SysStats.MemUsed.Val,
-		"cpu":           u.SystemStats.CPU.Val,
-		"mem":           u.SystemStats.Mem.Val,
-		"system_uptime": u.SystemStats.Uptime.Val,
+		"ip":            s.IP,
+		"bytes":         s.Bytes.Val,
+		"last_seen":     s.LastSeen.Val,
+		"rx_bytes":      s.RxBytes.Val,
+		"tx_bytes":      s.TxBytes.Val,
+		"uptime":        s.Uptime.Val,
+		"state":         s.State,
+		"user-num_sta":  int(s.UserNumSta.Val),
+		"guest-num_sta": int(s.GuestNumSta.Val),
+		"num_sta":       s.NumSta.Val,
+		"loadavg_1":     s.SysStats.Loadavg1.Val,
+		"loadavg_5":     s.SysStats.Loadavg5.Val,
+		"loadavg_15":    s.SysStats.Loadavg15.Val,
+		"mem_buffer":    s.SysStats.MemBuffer.Val,
+		"mem_total":     s.SysStats.MemTotal.Val,
+		"mem_used":      s.SysStats.MemUsed.Val,
+		"cpu":           s.SystemStats.CPU.Val,
+		"mem":           s.SystemStats.Mem.Val,
+		"system_uptime": s.SystemStats.Uptime.Val,
 		// Accumulative Statistics.
-		"stat_user-rx_packets":  u.Stat.Ap.UserRxPackets.Val,
-		"stat_guest-rx_packets": u.Stat.Ap.GuestRxPackets.Val,
-		"stat_rx_packets":       u.Stat.Ap.RxPackets.Val,
-		"stat_user-rx_bytes":    u.Stat.Ap.UserRxBytes.Val,
-		"stat_guest-rx_bytes":   u.Stat.Ap.GuestRxBytes.Val,
-		"stat_rx_bytes":         u.Stat.Ap.RxBytes.Val,
-		"stat_user-rx_errors":   u.Stat.Ap.UserRxErrors.Val,
-		"stat_guest-rx_errors":  u.Stat.Ap.GuestRxErrors.Val,
-		"stat_rx_errors":        u.Stat.Ap.RxErrors.Val,
-		"stat_user-rx_dropped":  u.Stat.Ap.UserRxDropped.Val,
-		"stat_guest-rx_dropped": u.Stat.Ap.GuestRxDropped.Val,
-		"stat_rx_dropped":       u.Stat.Ap.RxDropped.Val,
-		"stat_user-rx_crypts":   u.Stat.Ap.UserRxCrypts.Val,
-		"stat_guest-rx_crypts":  u.Stat.Ap.GuestRxCrypts.Val,
-		"stat_rx_crypts":        u.Stat.Ap.RxCrypts.Val,
-		"stat_user-rx_frags":    u.Stat.Ap.UserRxFrags.Val,
-		"stat_guest-rx_frags":   u.Stat.Ap.GuestRxFrags.Val,
-		"stat_rx_frags":         u.Stat.Ap.RxFrags.Val,
-		"stat_user-tx_packets":  u.Stat.Ap.UserTxPackets.Val,
-		"stat_guest-tx_packets": u.Stat.Ap.GuestTxPackets.Val,
-		"stat_tx_packets":       u.Stat.Ap.TxPackets.Val,
-		"stat_user-tx_bytes":    u.Stat.Ap.UserTxBytes.Val,
-		"stat_guest-tx_bytes":   u.Stat.Ap.GuestTxBytes.Val,
-		"stat_tx_bytes":         u.Stat.Ap.TxBytes.Val,
-		"stat_user-tx_errors":   u.Stat.Ap.UserTxErrors.Val,
-		"stat_guest-tx_errors":  u.Stat.Ap.GuestTxErrors.Val,
-		"stat_tx_errors":        u.Stat.Ap.TxErrors.Val,
-		"stat_user-tx_dropped":  u.Stat.Ap.UserTxDropped.Val,
-		"stat_guest-tx_dropped": u.Stat.Ap.GuestTxDropped.Val,
-		"stat_tx_dropped":       u.Stat.Ap.TxDropped.Val,
-		"stat_user-tx_retries":  u.Stat.Ap.UserTxRetries.Val,
-		"stat_guest-tx_retries": u.Stat.Ap.GuestTxRetries.Val,
+		"stat_user-rx_packets":  s.Stat.Ap.UserRxPackets.Val,
+		"stat_guest-rx_packets": s.Stat.Ap.GuestRxPackets.Val,
+		"stat_rx_packets":       s.Stat.Ap.RxPackets.Val,
+		"stat_user-rx_bytes":    s.Stat.Ap.UserRxBytes.Val,
+		"stat_guest-rx_bytes":   s.Stat.Ap.GuestRxBytes.Val,
+		"stat_rx_bytes":         s.Stat.Ap.RxBytes.Val,
+		"stat_user-rx_errors":   s.Stat.Ap.UserRxErrors.Val,
+		"stat_guest-rx_errors":  s.Stat.Ap.GuestRxErrors.Val,
+		"stat_rx_errors":        s.Stat.Ap.RxErrors.Val,
+		"stat_user-rx_dropped":  s.Stat.Ap.UserRxDropped.Val,
+		"stat_guest-rx_dropped": s.Stat.Ap.GuestRxDropped.Val,
+		"stat_rx_dropped":       s.Stat.Ap.RxDropped.Val,
+		"stat_user-rx_crypts":   s.Stat.Ap.UserRxCrypts.Val,
+		"stat_guest-rx_crypts":  s.Stat.Ap.GuestRxCrypts.Val,
+		"stat_rx_crypts":        s.Stat.Ap.RxCrypts.Val,
+		"stat_user-rx_frags":    s.Stat.Ap.UserRxFrags.Val,
+		"stat_guest-rx_frags":   s.Stat.Ap.GuestRxFrags.Val,
+		"stat_rx_frags":         s.Stat.Ap.RxFrags.Val,
+		"stat_user-tx_packets":  s.Stat.Ap.UserTxPackets.Val,
+		"stat_guest-tx_packets": s.Stat.Ap.GuestTxPackets.Val,
+		"stat_tx_packets":       s.Stat.Ap.TxPackets.Val,
+		"stat_user-tx_bytes":    s.Stat.Ap.UserTxBytes.Val,
+		"stat_guest-tx_bytes":   s.Stat.Ap.GuestTxBytes.Val,
+		"stat_tx_bytes":         s.Stat.Ap.TxBytes.Val,
+		"stat_user-tx_errors":   s.Stat.Ap.UserTxErrors.Val,
+		"stat_guest-tx_errors":  s.Stat.Ap.GuestTxErrors.Val,
+		"stat_tx_errors":        s.Stat.Ap.TxErrors.Val,
+		"stat_user-tx_dropped":  s.Stat.Ap.UserTxDropped.Val,
+		"stat_guest-tx_dropped": s.Stat.Ap.GuestTxDropped.Val,
+		"stat_tx_dropped":       s.Stat.Ap.TxDropped.Val,
+		"stat_user-tx_retries":  s.Stat.Ap.UserTxRetries.Val,
+		"stat_guest-tx_retries": s.Stat.Ap.GuestTxRetries.Val,
 	}
-	pt, err := influx.NewPoint("uap", tags, fields, now)
-	if err != nil {
-		return nil, err
-	}
-	morePoints, err := processVAPs(u.VapTable, u.RadioTable, u.RadioTableStats, u.Name, u.Mac, u.SiteName, now)
-	if err != nil {
-		return nil, err
-	}
-	return append(morePoints, pt), nil
+	r.send(&metric{Table: "uap", Tags: tags, Fields: fields})
+	u.processVAPs(r, s.VapTable, s.RadioTable, s.RadioTableStats, s.Name, s.Mac, s.SiteName)
 }
 
 // processVAPs creates points for Wifi Radios. This works with several types of UAP-capable devices.
-func processVAPs(vt unifi.VapTable, rt unifi.RadioTable, rts unifi.RadioTableStats, name, mac, sitename string, ts time.Time) ([]*influx.Point, error) {
-	tags := make(map[string]string)
-	fields := make(map[string]interface{})
-	points := []*influx.Point{}
-
+func (u *InfluxUnifi) processVAPs(r report, vt unifi.VapTable, rt unifi.RadioTable, rts unifi.RadioTableStats, name, mac, sitename string) {
 	// Loop each virtual AP (ESSID) and extract data for it
 	// from radio_tables and radio_table_stats.
 	for _, s := range vt {
+		tags := make(map[string]string)
+		fields := make(map[string]interface{})
 		tags["device_name"] = name
 		tags["device_mac"] = mac
 		tags["site_name"] = sitename
@@ -113,7 +99,6 @@ func processVAPs(vt unifi.VapTable, rt unifi.RadioTable, rts unifi.RadioTableSta
 		tags["is_guest"] = s.IsGuest.Txt
 
 		fields["ccq"] = s.Ccq
-		fields["extchannel"] = s.Extchannel
 		fields["mac_filter_rejections"] = s.MacFilterRejections
 		fields["num_satisfaction_sta"] = s.NumSatisfactionSta.Val
 		fields["avg_client_signal"] = s.AvgClientSignal.Val
@@ -185,12 +170,6 @@ func processVAPs(vt unifi.VapTable, rt unifi.RadioTable, rts unifi.RadioTableSta
 			fields["tx_retries"] = p.TxRetries.Val
 			fields["user-num_sta"] = p.UserNumSta.Val
 		}
-
-		pt, err := influx.NewPoint("uap_vaps", tags, fields, ts)
-		if err != nil {
-			return points, err
-		}
-		points = append(points, pt)
+		r.send(&metric{Table: "uap_vaps", Tags: tags, Fields: fields})
 	}
-	return points, nil
 }
