f0847c8e11b30b952d9558579b1aa05c77390558
davidnewhall2 <david@sleepers.pro>
2019-12-03 02:44:14 -0800

    still testing

--- scripts/package-deploy.sh
+++ scripts/package-deploy.sh
@@ -8,12 +8,12 @@ echo "deploy source branch: $SOURCE_BRANCH"
 
 source .metadata.sh
 # deb
-package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}.arm64.deb
-package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}.amd64.deb
-package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}.armhf.deb
-package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}.i386.deb
+package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}-${ITERATION}.arm64.deb
+package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}-${ITERATION}.amd64.deb
+package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}-${ITERATION}.armhf.deb
+package_cloud push golift/${REPO}/debian/stretch release/unifi-poller_${VERSION}-${ITERATION}.i386.deb
 # rpm
-package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}.arm64.rpm
-package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}.amd64.rpm
-package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}.armhf.rpm
-package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}.i386.rpm
+package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}-${ITERATION}.arm64.rpm
+package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}-${ITERATION}.amd64.rpm
+package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}-${ITERATION}.armhf.rpm
+package_cloud push golift/${REPO}/el/5 release/unifi-poller-${VERSION}-${ITERATION}.i386.rpm
