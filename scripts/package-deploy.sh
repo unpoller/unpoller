d14ee0aae0171984cdb9d92e331c6dbfaa5a146c
davidnewhall2 <david@sleepers.pro>
2019-12-03 03:37:55 -0800

    do is required

--- scripts/package-deploy.sh
+++ scripts/package-deploy.sh
@@ -8,13 +8,13 @@ echo "deploying packages from branch: $TRAVIS_BRANCH, tag: $TRAVIS_TAG to repo: 
 
 source .metadata.sh
 # deb
-cmd="rvm 2.0.0 package_cloud push golift/${REPO}/debian/stretch"
+cmd="rvm 2.0.0 do package_cloud push golift/${REPO}/debian/stretch"
 $cmd release/unifi-poller_${VERSION}-${ITERATION}_arm64.deb
 $cmd release/unifi-poller_${VERSION}-${ITERATION}_amd64.deb
 $cmd release/unifi-poller_${VERSION}-${ITERATION}_armhf.deb
 $cmd release/unifi-poller_${VERSION}-${ITERATION}_i386.deb
 # rpm
-cmd="rvm 2.0.0 package_cloud push golift/${REPO}/el/5"
+cmd="rvm 2.0.0 do package_cloud push golift/${REPO}/el/5"
 $cmd release/unifi-poller-${VERSION}-${ITERATION}.arm64.rpm
 $cmd release/unifi-poller-${VERSION}-${ITERATION}.x86_64.rpm
 $cmd release/unifi-poller-${VERSION}-${ITERATION}.armhf.rpm
