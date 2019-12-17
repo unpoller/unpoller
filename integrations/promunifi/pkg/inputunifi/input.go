c8276219669e9e4be42b6b0b0f84c706a7cf9c3e
davidnewhall2 <david@sleepers.pro>
2019-12-17 01:31:30 -0800

    make dumper work

--- integrations/promunifi/pkg/inputunifi/input.go
+++ integrations/promunifi/pkg/inputunifi/input.go
@@ -4,6 +4,8 @@ package inputunifi
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"sync"
 
@@ -13,7 +15,7 @@ import (
 
 // InputUnifi contains the running data.
 type InputUnifi struct {
-	Config Config `json:"unifi" toml:"unifi" xml:"unifi" yaml:"unifi"`
+	Config *Config `json:"unifi" toml:"unifi" xml:"unifi" yaml:"unifi"`
 	poller.Logger
 }
 
@@ -33,9 +35,9 @@ type Controller struct {
 
 // Config contains our configuration data
 type Config struct {
-	sync.RWMutex              // locks the Unifi struct member when re-authing to unifi.
-	Disable      bool         `json:"disable" toml:"disable" xml:"disable" yaml:"disable"`
-	Controllers  []Controller `json:"controllers" toml:"controller" xml:"controller" yaml:"controllers"`
+	sync.RWMutex               // locks the Unifi struct member when re-authing to unifi.
+	Disable      bool          `json:"disable" toml:"disable" xml:"disable" yaml:"disable"`
+	Controllers  []*Controller `json:"controllers" toml:"controller" xml:"controller" yaml:"controllers"`
 }
 
 func init() {
@@ -48,7 +50,7 @@ func init() {
 }
 
 // getUnifi (re-)authenticates to a unifi controller.
-func (u *InputUnifi) getUnifi(c Controller) error {
+func (u *InputUnifi) getUnifi(c *Controller) error {
 	var err error
 
 	u.Config.Lock()
@@ -75,4 +77,75 @@ func (u *InputUnifi) getUnifi(c Controller) error {
 	u.LogDebugf("Authenticated with controller successfully, %s", c.URL)
 
 	return nil
+}
+
+// checkSites makes sure the list of provided sites exists on the controller.
+// This only runs once during initialization.
+func (u *InputUnifi) checkSites(c *Controller) error {
+	u.Config.RLock()
+	defer u.Config.RUnlock()
+
+	if len(c.Sites) < 1 || c.Sites[0] == "" {
+		c.Sites = []string{"all"}
+	}
+
+	u.LogDebugf("Checking Controller Sites List")
+
+	sites, err := c.Unifi.GetSites()
+	if err != nil {
+		return err
+	}
+
+	msg := []string{}
+	for _, site := range sites {
+		msg = append(msg, site.Name+" ("+site.Desc+")")
+	}
+
+	u.Logf("Found %d site(s) on controller %s: %v", len(msg), c.Name, strings.Join(msg, ", "))
+
+	if StringInSlice("all", c.Sites) {
+		c.Sites = []string{"all"}
+		return nil
+	}
+
+FIRST:
+	for _, s := range c.Sites {
+		for _, site := range sites {
+			if s == site.Name {
+				continue FIRST
+			}
+		}
+		return fmt.Errorf("configured site not found on controller: %v", s)
+	}
+
+	return nil
+}
+
+func (u *InputUnifi) dumpSitesJSON(c *Controller, path, name string, sites unifi.Sites) ([]byte, error) {
+	allJSON := []byte{}
+
+	for _, s := range sites {
+		apiPath := fmt.Sprintf(path, s.Name)
+		_, _ = fmt.Fprintf(os.Stderr, "[INFO] Dumping %s: '%s' JSON for site: %s (%s):\n", name, apiPath, s.Desc, s.Name)
+
+		body, err := c.Unifi.GetJSON(apiPath)
+		if err != nil {
+			return allJSON, err
+		}
+
+		allJSON = append(allJSON, body...)
+	}
+
+	return allJSON, nil
+}
+
+// StringInSlice returns true if a string is in a slice.
+func StringInSlice(str string, slice []string) bool {
+	for _, s := range slice {
+		if strings.EqualFold(s, str) {
+			return true
+		}
+	}
+
+	return false
 }
