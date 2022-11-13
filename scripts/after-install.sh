a6673b4d42e52af9547f11e8709403e936ca0d54
Cody Lee <cody.lee@datadoghq.com>
2022-11-13 14:39:33 -0600

    fix missing docs, add missing license
    fix broken pre/post scripts for packages

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -9,21 +9,21 @@ OS="$(uname -s)"
 
 if [ "${OS}" = "Linux" ]; then
   # Make a user and group for this app, but only if it does not already exist.
-  id {{BINARY}} >/dev/null 2>&1  || \
-    useradd --system --user-group --no-create-home --home-dir /tmp --shell /bin/false {{BINARY}}
+  id unpoller >/dev/null 2>&1  || \
+    useradd --system --user-group --no-create-home --home-dir /tmp --shell /bin/false unpoller
 elif [ "${OS}" = "OpenBSD" ]; then
-  id {{BINARY}} >/dev/null 2>&1  || \
-    useradd  -g =uid -d /tmp -s /bin/false {{BINARY}}
+  id unpoller >/dev/null 2>&1  || \
+    useradd  -g =uid -d /tmp -s /bin/false unpoller
 elif [ "${OS}" = "FreeBSD" ]; then
-  id {{BINARY}} >/dev/null 2>&1  || \
-    pw useradd {{BINARY}} -d /tmp -w no -s /bin/false
+  id unpoller >/dev/null 2>&1  || \
+    pw useradd unpoller -d /tmp -w no -s /bin/false
 else
-  echo "Unknown OS: ${OS}, please add system user {{BINARY}} manually."
+  echo "Unknown OS: ${OS}, please add system user unpoller manually."
 fi
 
 if [ -x "/bin/systemctl" ]; then
   # Reload and restart - this starts the application as user nobody.
   /bin/systemctl daemon-reload
-  /bin/systemctl enable {{BINARY}}
-  /bin/systemctl restart {{BINARY}}
+  /bin/systemctl enable unpoller
+  /bin/systemctl restart unpoller
 fi
