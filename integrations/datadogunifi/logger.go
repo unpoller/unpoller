2164d6f502dbf1f68d075e5d4296fc7671a62338
Cody Lee <cody.lee@datadoghq.com>
2022-11-23 18:07:38 -0600

    nil guard when disabled datadog plugin for logging

--- integrations/datadogunifi/logger.go
+++ integrations/datadogunifi/logger.go
@@ -2,15 +2,21 @@ package datadogunifi
 
 // Logf logs a message.
 func (u *DatadogUnifi) Logf(msg string, v ...interface{}) {
-	u.Collector.Logf(msg, v...)
+	if u.Collector != nil {
+		u.Collector.Logf(msg, v...)
+	}
 }
 
 // LogErrorf logs an error message.
 func (u *DatadogUnifi) LogErrorf(msg string, v ...interface{}) {
-	u.Collector.LogErrorf(msg, v...)
+	if u.Collector != nil {
+		u.Collector.LogErrorf(msg, v...)
+	}
 }
 
 // LogDebugf logs a debug message.
 func (u *DatadogUnifi) LogDebugf(msg string, v ...interface{}) {
-	u.Collector.LogDebugf(msg, v...)
+	if u.Collector != nil {
+		u.Collector.LogDebugf(msg, v...)
+	}
 }
