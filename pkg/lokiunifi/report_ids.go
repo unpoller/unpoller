6ee728cba6342631c6d8c66768200a6de5a25d3e
Cody Lee <cody.lee@datadoghq.com>
2022-12-11 14:29:13 -0600

    add additional source data for issue #414

--- pkg/lokiunifi/report_ids.go
+++ pkg/lokiunifi/report_ids.go
@@ -15,23 +15,26 @@ func (r *Report) IDS(event *unifi.IDS, logs *Logs) {
 	}
 
 	r.Counts[typeIDS]++ // increase counter and append new log line.
-
+	
 	logs.Streams = append(logs.Streams, LogStream{
 		Entries: [][]string{{strconv.FormatInt(event.Datetime.UnixNano(), 10), event.Msg}},
 		Labels: CleanLabels(map[string]string{
-			"application":  "unifi_ids",
-			"source":       event.SourceName,
-			"site_name":    event.SiteName,
-			"subsystem":    event.Subsystem,
-			"category":     event.Catname,
-			"event_type":   event.EventType,
-			"key":          event.Key,
-			"app_protocol": event.AppProto,
-			"protocol":     event.Proto,
-			"interface":    event.InIface,
-			"src_country":  event.SrcIPCountry,
-			"usgip":        event.USGIP,
-			"action":       event.InnerAlertAction,
+			"application":      "unifi_ids",
+			"source":           event.SourceName,
+			"site_name":        event.SiteName,
+			"subsystem":        event.Subsystem,
+			"category":         event.Catname,
+			"event_type":       event.EventType,
+			"key":              event.Key,
+			"app_protocol":     event.AppProto,
+			"protocol":         event.Proto,
+			"interface":        event.InIface,
+			"src_country":      event.SrcIPCountry,
+			"src_city":         event.SourceIPGeo.City,
+			"src_continent":    event.SourceIPGeo.ContinentCode,
+			"src_country_code": event.SourceIPGeo.CountryCode,
+			"usgip":            event.USGIP,
+			"action":           event.InnerAlertAction,
 		}),
 	})
 }
